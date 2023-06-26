f9c4fd151b6f2c23aae8b446ec94daad80540202
yeomjinseop <yeomjinseop@gmail.com>
2023-06-26 15:10:15 +0900

    remove .history dir

--- .history/app_20230626141213.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import 'dotenv';
-
-const app = express();
-console.log(process.env.OPENAI_API_KEY);
-
-app.listen(80);
--- .history/app_20230626141244.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { config } from 'dotenv';
-
-config();
-const app = express();
-console.log(process.env.OPENAI_API_KEY);
-
-app.listen(80);
